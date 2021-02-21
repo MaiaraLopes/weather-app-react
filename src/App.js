e937ebf79510e1bb48f0734a3ec34aff348437ca
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2021-02-21 17:33:30 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      Weather app
      <footer>
        This project was coded by Maiara Lopes and is <a href="https://github.com/MaiaraLopes/final-project" target="_blank">open-sourced.
      </a>
     </footer>
+    </div>
     </div>
     
   );
