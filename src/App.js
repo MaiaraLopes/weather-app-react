979de657679ed066fc7b3deca93c57b060460dc2
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2021-02-21 15:07:40 +0000

    Added the GitHub URL

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+     Weather app
+     <footer>
+       This project was coded by Maiara Lopes and is <a href="https://github.com/MaiaraLopes/final-project" target="_blank">open-sourced.
+     </a>
+    </footer>
+    </div>
+    
+  );
+}
